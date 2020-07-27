5d7515b0e7740e48a8234f1f54ff17153037b10c
007steve <stephen.plummer1.sp@gmail.com>
2020-07-27 16:13:21 -0400

    added fetch and change to a class component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,38 +1,29 @@
-import React from 'react';
-import Movies from './Movies.js'
+import React,{Component} from 'react';
+import Movies from './Movies.js';
 import './App.css';
-const movies = [
-  {
-    id: "1",
-    title: "lion king",
-    image: "https://lumiere-a.akamaihd.net/v1/images/b_thelionking2019_header_poststreet_mobile_18276_8dd5ba33.jpeg?region=0,0,640,430",
-    length:"1"
-  },
-  {
-    id: "2",
-    title: "hamiltion",
-    image: "https://www.theatermania.com/dyn/graphics/theatermania/v2w2400/hamilton-logo-85066.jpeg",
-    length:"2"
-  },
-  {
-    id: "3",
-    title: "space movies",
-    image: "https://images-na.ssl-images-amazon.com/images/I/814WU+gsXTL._RI_.jpg",
-    length:"145"
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+  
+    this.state = { 
+      movies:[]
+    }
   }
-]
- 
-
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <Movies movies={movies}/>
-      
-    </div>
-  );
+  componentDidMount(){
+    fetch("http://localhost:3000/movies")
+    .then(response => response.json())
+    .then(movies => this.setState({movies}))
+  }
+  render() { 
+    
+    return ( <div className="App">
+    <Movies movies={this.state.movies}/>
+    
+  </div> );
+  }
 }
+ 
+export default App ;
 
-export default App;
