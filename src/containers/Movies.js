8acfd48d90644b69f47a7094f63dc1e6ec63f953
007steve <stephen.plummer1.sp@gmail.com>
2020-07-27 16:44:47 -0400

    moved to MovieCard and add className

--- src/containers/Movies.js
+++ src/containers/Movies.js
@@ -1,17 +1,11 @@
 import React from 'react';
+import MovieCard from '../components/MovieCard'
 import './Movies.css'
 const Movies = (props) => (
-       <div>
+       <div className="Movie-Container">
          <h1>Movies component</h1>
-         {props.movies.map(movie =>
-            <div key={movie.id} className="Movie-Card">
-                <img className="Movie-Image" src={movie.image} alt={movie.title}/>
-                <h1>{movie.title}</h1>
-                <p>{movie.description}</p>
-                <h3>{movie.category}</h3>
-                <h3>{movie.year}</h3>
-                
-            </div>
+         {props.movies.map(movie => <MovieCard movie={movie}/>
+            
             )}
         </div> 
       );
