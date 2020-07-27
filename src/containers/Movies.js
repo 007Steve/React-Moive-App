30728c701b3fa1945c60df0f6fd5406b99bf23b6
007steve <stephen.plummer1.sp@gmail.com>
2020-07-27 16:11:23 -0400

    updated movie card added key and fix description

--- src/containers/Movies.js
+++ src/containers/Movies.js
@@ -4,10 +4,10 @@ const Movies = (props) => (
        <div>
          <h1>Movies component</h1>
          {props.movies.map(movie =>
-            <div className="Movie-Card">
+            <div key={movie.id} className="Movie-Card">
                 <img className="Movie-Image" src={movie.image} alt={movie.title}/>
                 <h1>{movie.title}</h1>
-                <h3>{movie.desrciption}</h3>
+                <p>{movie.description}</p>
                 <h3>{movie.category}</h3>
                 <h3>{movie.year}</h3>
                 
