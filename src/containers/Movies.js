351861208ebd5d993388563d4ddb2bcdc135f190
007steve <stephen.plummer1.sp@gmail.com>
2020-07-27 22:50:14 -0400

    testing redux store

--- src/containers/Movies.js
+++ src/containers/Movies.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import MovieCard from '../components/MovieCard'
-import './Movies.css'
-const Movies = (props) => (
-       <div className="Movie-Container">
-         <h1>Movies component</h1>
-         {props.movies.map(movie => <MovieCard movie={movie}/>
-            
-            )}
-        </div> 
-      );
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import MovieCard from '../components/MovieCard';
+import './Movies.css';
+import MovieForm from '../containers/MovieForm';
+class Movies extends Component {
+componentDidMount(){
+    this.props.dispatch({
+        type: 'GET_MOVIES_SUCCESS',
+        movies: [{ title: "Mars", image: "https://m.media-amazon.com/images/M/MV5BMTc2MTQ3MDA1Nl5BMl5BanBnXkFtZTgwODA3OTI4NjE@._V1_.jpg", description: " lorem ips  fgggf", category: "sci-fi", year:"2015" }]
+    })
+}
+    render(){
+        return(
+            <div className="Movie-Container">
+              <h1>Movies component</h1>
+                  {this.props.movies.map(movie => <MovieCard key={movie.id} movie={movie}/>
+                )}
+                <MovieForm/>
+           </div> 
+        )
+    }
+}
+const mapStateToProps = (state) => {
+    return ({
+        movies: state.movies
+    })
+}
 
-  export default Movies;
+  export default connect(mapStateToProps)(Movies);
