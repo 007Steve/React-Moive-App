56fab09407f11640e1099408ef205c110dff005a
007steve <stephen.plummer1.sp@gmail.com>
2020-07-27 22:51:37 -0400

    clean up app component

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,22 +4,16 @@ import './App.css';
 
 
 class App extends Component {
-  constructor(props){
-    super(props)
   
-    this.state = { 
-      movies:[]
-    }
-  }
   componentDidMount(){
     fetch("http://localhost:3000/movies")
     .then(response => response.json())
-    .then(movies => this.setState({movies}))
+   
   }
   render() { 
     
     return ( <div className="App">
-    <Movies movies={this.state.movies}/>
+    <Movies />
     
   </div> );
   }
